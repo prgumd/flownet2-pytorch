Fix resuming epoch off by one

main.py
@@ -218,7 +218,7 @@ if __name__ == '__main__':
             block.log("Loading checkpoint '{}'".format(args.resume))
             checkpoint = torch.load(args.resume)
             if not args.inference:
-                args.start_epoch = checkpoint['epoch']
+                args.start_epoch = checkpoint['epoch'] + 1
             best_err = checkpoint['best_EPE']
             model_and_loss.module.model.load_state_dict(checkpoint['state_dict'])
             block.log("Loaded checkpoint '{}' (at epoch {})".format(args.resume, checkpoint['epoch']))